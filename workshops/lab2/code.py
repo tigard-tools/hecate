b054bb5456aefbd9679d90b272e6525fddfa215d
hecate-dev <maxiecorbin@protonmail.com>
2026-08-08 06:48:56 -0700

    fix: more detailed comments in lab 2

--- workshops/lab2/code.py
+++ workshops/lab2/code.py
@@ -4,8 +4,12 @@ from hecate import Hecate, Trigger
 
 class CodeReplay:
     def __init__(self, terminators=b"\r\n"):
+        # We are setting up some internal state for CodeReplay here
+        # terminators is a list of bytes that signal the beginning or end of a message
         self._terminators = terminators
+        # buf is the buffer where we'll store the code we want to replay
         self._buf = bytearray()
+        # and finally, last_code is where we'll store the code we've seen most recently
         self.last_code = None
 
     def write(self, data):
@@ -14,9 +18,10 @@ class CodeReplay:
         pass
 
     def replay(self, hecate, wire):
-        # TODO: replay the most recent code you've seen back onto the wire so a
-        # button press re-sends it
-        pass
+        if self.last_code:
+            # When we call replay on a specific wire, we'll take our most recent code
+            # and send it in UART along that wire
+            wire.inject(self.last_code)
 
 
 h = Hecate(
